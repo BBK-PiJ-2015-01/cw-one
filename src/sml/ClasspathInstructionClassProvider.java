95aef32dc9719dd87494265bdb28d2a0c9e21a32
sbaird02 <sbaird02@mail.bbk.ac.uk>
2016-02-01 07:35:49 +0000

    Created Classpath provider

---
+++ src/sml/ClasspathInstructionClassProvider.java
@@ -0,0 +1,13 @@
+package sml;
+
+import java.util.Collection;
+
+public class ClasspathInstructionClassProvider implements InstructionClassProvider {
+
+	@Override
+	public Collection<Class<?>> getClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
